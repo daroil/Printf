22ebbcabca9719f85e59d3bba58c8fbc5e5a3c41
Danila <gendel2403@gmail.com>
2022-11-11 06:39:55 +0100

    updated header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dhendzel <dhendzel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 17:52:40 by dhendzel          #+#    #+#             */
-/*   Updated: 2022/10/19 16:41:00 by dhendzel         ###   ########.fr       */
+/*   Updated: 2022/11/11 06:39:11 by dhendzel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int		ft_putchar(char c);
 int		ft_putstr(char *s);
 int		ft_putnbr(int n);
 int		ft_printf(const char *str, ...);
-size_t	ft_count_num(int n);
 int		ft_print_hexa(unsigned int u, char c);
 int		ft_print_pointer(unsigned long ptr);
 int		ft_put_unsigned_nbr(unsigned int n);
